876cb72d34cd57d5c2fca276c95e88f9549b0a05
luketparsons@gmail.com <luketparsons@clearswift.com>
2024-01-02 13:05:54 +0000

    feat: day2 part2 was easy because of how I did part 1. I'm amazing

--- src/day02/index.ts
+++ src/day02/index.ts
@@ -1,5 +1,5 @@
-import run from 'aocrunner'
-import { match } from 'node:assert'
+import run from "aocrunner"
+import { match } from "node:assert"
 
 type highestRGB = {
   gameId: number
@@ -22,10 +22,10 @@ const extractGameIdFromStr = (input: string) => {
 
 const returnHighestOccurrenceOfColour = (
   input: string,
-  colour: 'red' | 'green' | 'blue',
+  colour: "red" | "green" | "blue",
 ) => {
   const matches = [
-    ...input.matchAll(new RegExp(`\\b(\\d+)\\s${colour}\\b`, 'g')),
+    ...input.matchAll(new RegExp(`\\b(\\d+)\\s${colour}\\b`, "g")),
   ].map((match) => parseInt(match[1]))
   return (
     matches?.reduce(
@@ -38,21 +38,21 @@ const returnHighestOccurrenceOfColour = (
 
 const parseInput = (rawInput: string): highestRGB[] => {
   let parsedHighest: highestRGB[] = []
-  const games = rawInput.split('\n')
+  const games = rawInput.split("\n")
   games.map((game) => {
-    const splitGameFromSubsets = game.split(':')
+    const splitGameFromSubsets = game.split(":")
     const gameId = extractGameIdFromStr(splitGameFromSubsets[0])
     const highestRed = returnHighestOccurrenceOfColour(
       splitGameFromSubsets[1],
-      'red',
+      "red",
     )
     const highestGreen = returnHighestOccurrenceOfColour(
       splitGameFromSubsets[1],
-      'green',
+      "green",
     )
     const highestBlue = returnHighestOccurrenceOfColour(
       splitGameFromSubsets[1],
-      'blue',
+      "blue",
     )
     const highestRgb = {
       gameId,
@@ -70,16 +70,16 @@ const part1 = (rawInput: string) => {
   const numCubes = { red: 12, green: 13, blue: 14 }
 
   return input.reduce((acc, currentValue) => {
-    const keys = Object.keys(currentValue.rgb) as Array<keyof highestRGB['rgb']>
+    const keys = Object.keys(currentValue.rgb) as Array<keyof highestRGB["rgb"]>
 
     for (let key of keys) {
       if (currentValue.rgb[key] > numCubes[key]) {
-        console.log(`skipping gameId: ${currentValue.gameId}`)
+        // console.log(`skipping gameId: ${currentValue.gameId}`)
         return acc
       }
     }
 
-    console.log(`gameId: ${currentValue.gameId} added to sum`)
+    // console.log(`gameId: ${currentValue.gameId} added to sum`)
     return acc + currentValue.gameId
   }, 0)
 }
@@ -87,7 +87,12 @@ const part1 = (rawInput: string) => {
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput)
 
-  return
+  // part 2 just requires getting the highest occurence of each colour cube
+  // from each game and then multiplying them together. Then adding all those
+  // Sums together
+  return input.reduce((acc, { rgb: { red, green, blue } }) => {
+    return acc + red * green * blue
+  }, 0)
 }
 
 run({
@@ -95,11 +100,11 @@ run({
     tests: [
       {
         input:
-          'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\n' +
-          'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\n' +
-          'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\n' +
-          'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\n' +
-          'Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green',
+          "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\n" +
+          "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\n" +
+          "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\n" +
+          "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\n" +
+          "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
         expected: 8,
       },
     ],
@@ -107,10 +112,14 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\n" +
+          "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\n" +
+          "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\n" +
+          "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\n" +
+          "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+        expected: 2286,
+      },
     ],
     solution: part2,
   },
